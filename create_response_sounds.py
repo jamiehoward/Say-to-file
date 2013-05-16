44c8b44c04b021d38908b7743adce9f92e1de868
Jamie Howard <jamie.howard@blackairplane.com>
2013-05-16 14:22:53 -0400

    Convert aiff to mp3 files

--- create_response_sounds.py
+++ create_response_sounds.py
@@ -1,9 +1,11 @@
 import os
 
 # Configuration options
-command_list = '/' 
-save_path = '/'
-file_extension = '.aiff'
+command_list = '/audio_list'
+save_path = '/audio/mp3/'
+tmp_path = '/audio/tmp/'
+file_extension = '.mp3'
+tmp_extension = '.aiff'
 voice = 'Lee' # Lee (Australian), Daniel (UK), Alex (US)
 
 # Get the command list
@@ -20,16 +22,23 @@ successes = []
 for command in commands:
 	command = command.rstrip('\n')
 	command_text = command
-	command_file = command + file_extension
+	tmp_file = command
 
 	# Fix punctuation and spaces
 	if (' ' in command) == True:
-		command_file = command_file.replace(" ", "_")
-	if ("'" in command == True):
-		command_file = command_file.replace("'", "")
+		tmp_file = tmp_file.replace(" ", "_")
+	if ("'" in command) == True:
+		tmp_file = tmp_file.replace("'", "")
 		command_text = command_text.replace("'", "\'")
-	if (',' in command == True):
-		command_file = command_file.replace(',', "")
+	if (',' in command) == True:
+		tmp_file = tmp_file.replace(',', "")
+	if ('?' in command) == True:
+		tmp_file = tmp_file.replace('?', "")
+	if ('.' in command) == True:
+		tmp_file = tmp_file.replace('.', "")
+
+	command_file = tmp_file + file_extension
+	tmp_file = tmp_file + tmp_extension
 	
 	# Create file only if it does not exist
 	try:
@@ -37,10 +46,15 @@ for command in commands:
 			#print 'File exists'
 			pass
 	except:
-		terminal_command = 'say -v' + voice + ' -o"' + save_path + command_file + '" "' + command_text + '"'
+		tmp_command = 'say -v' + voice + ' -o"' + tmp_path + tmp_file + '" "' + command_text + '"'
+		lame_command = 'lame --quiet ' + tmp_path + tmp_file + ' ' + save_path + command_file
 		try:
-			os.system(terminal_command)
-			print terminal_command
+			os.system(tmp_command)
+			os.system(lame_command)
+			os.system('rm -f ' + tmp_path + tmp_file)
+			# print tmp_command
+			print lame_command
+			# print "\n"
 			successes.append(command_file)
 		except IOError:
 			print 'File ' + command_file + ' could not be created!';
